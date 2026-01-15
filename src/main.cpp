9f8a7c96fc52fa2712db0d8ca99dafcd36884b9a
Kiyum <kiyum@Kiyums-MacBook-Air.local>
2026-01-15 11:52:21 -0800

    Added some comments

--- src/main.cpp
+++ src/main.cpp
@@ -9,18 +9,22 @@ int main() {
 
 
     while(true) {
+        //a button
         if (bn::keypad::a_pressed()) {
             bn::backdrop::set_color(bn::color(31, 21, 22));
         }
 
+        //b button
         if (bn::keypad::b_pressed()) {
             bn::backdrop::set_color(bn::color(31, 0, 0));
         }
 
+        //left button
         if (bn::keypad::left_released()) {
             bn::backdrop::set_color(bn::color(0, 0, 15));
         }
 
+        //right button
         if (bn::keypad::right_released()) {
             bn::backdrop::set_color(bn::color(31, 31, 0));
         }
