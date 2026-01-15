833993c22bb48b3beb0f50a7fa99145fbe9b7cd3
Kiyum <kiyum@Kiyums-MacBook-Air.local>
2026-01-15 10:45:39 -0800

    I have added colors for a-button and b-button

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,24 @@
 #include <bn_backdrop.h>
 #include <bn_color.h>
 #include <bn_core.h>
+#include <bn_keypad.h>
 
 int main() {
     bn::core::init();
-    bn::backdrop::set_color(bn::color(3, 30, 19));
+    bn::backdrop::set_color(bn::color(3, 6, 9));
 
 
     while(true) {
+        if (bn::keypad::a_pressed()) {
+            bn::backdrop::set_color(bn::color(31, 21, 22));
+        }
+
+        if (bn::keypad::b_pressed()) {
+            bn::backdrop::set_color(bn::color(31, 0, 0));
+        }
+
         bn::core::update();
     }
+
+    
 }
