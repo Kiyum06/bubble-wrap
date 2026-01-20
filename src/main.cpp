4b8096b17c8b70820daf55035d9b26f123043be2
Kiyum <kiyum@Kiyums-MacBook-Air.local>
2026-01-19 22:58:58 -0800

    successfully made a line of spprites.

--- src/main.cpp
+++ src/main.cpp
@@ -10,10 +10,20 @@ int main() {
     bn::core::init();
     bn::backdrop::set_color(bn::color(3, 6, 9));
 
-    bn::sprite_ptr myCircle = bn::sprite_items::dot.create_sprite(20, 20);
-    bn::sprite_ptr myCircle2 = bn::sprite_items::dot.create_sprite(20, -20);
-    bn::sprite_ptr myCircle3 = bn::sprite_items::dot.create_sprite(-20, 20);
-    bn::sprite_ptr myCircle4 = bn::sprite_items::dot.create_sprite(-20, -20);
+    // bn::sprite_ptr myCircle = bn::sprite_items::dot.create_sprite(20, 20);
+    // bn::sprite_ptr myCircle2 = bn::sprite_items::dot.create_sprite(20, -20);
+    // bn::sprite_ptr myCircle3 = bn::sprite_items::dot.create_sprite(-20, 20);
+    // bn::sprite_ptr myCircle4 = bn::sprite_items::dot.create_sprite(-20, -20);
+
+    bn::sprite_ptr myCircle = bn::sprite_items::dot.create_sprite(-40, 40);
+    bn::sprite_ptr myCircle2 = bn::sprite_items::dot.create_sprite(-30, 40);
+    bn::sprite_ptr myCircle3 = bn::sprite_items::dot.create_sprite(-20, 40);
+    bn::sprite_ptr myCircle4 = bn::sprite_items::dot.create_sprite(-10, 40);
+    bn::sprite_ptr myCircle5 = bn::sprite_items::dot.create_sprite(0, 40);
+    bn::sprite_ptr myCircle6 = bn::sprite_items::dot.create_sprite(10, 40);
+    bn::sprite_ptr myCircle7 = bn::sprite_items::dot.create_sprite(20, 40);
+    bn::sprite_ptr myCircle8 = bn::sprite_items::dot.create_sprite(30, 40);
+    bn::sprite_ptr myCircle9 = bn::sprite_items::dot.create_sprite(40, 40);
     
 
 
